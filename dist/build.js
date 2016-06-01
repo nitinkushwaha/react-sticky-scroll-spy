d980ac408c48f2b138cd0e8964b4819273b7a1c0
Nitin kushwaha <talktonitin@gmail.com>
2016-06-01 15:37:21 +0530

    final release

--- dist/build.js
+++ dist/build.js
@@ -4,15 +4,11 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
-var _react2 = require('react');
-
-var _react3 = _interopRequireDefault(_react2);
-
-var _reactTransformHmr3 = require('react-transform-hmr');
-
-var _reactTransformHmr4 = _interopRequireDefault(_reactTransformHmr3);
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -22,26 +18,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _components = {
-	ReactStickyScrollSpy: {
-		displayName: 'ReactStickyScrollSpy'
-	}
-};
-
-var _reactTransformHmr2 = (0, _reactTransformHmr4.default)({
-	filename: 'src/react-sticky-scroll-spy.js',
-	components: _components,
-	locals: [module],
-	imports: [_react3.default]
-});
-
-function _wrapComponent(id) {
-	return function (Component) {
-		return _reactTransformHmr2(Component, id);
-	};
-}
-
-var ReactStickyScrollSpy = _wrapComponent('ReactStickyScrollSpy')(function (_React$Component) {
+var ReactStickyScrollSpy = function (_React$Component) {
 	_inherits(ReactStickyScrollSpy, _React$Component);
 
 	function ReactStickyScrollSpy(props) {
@@ -191,10 +168,10 @@ var ReactStickyScrollSpy = _wrapComponent('ReactStickyScrollSpy')(function (_Rea
 	}, {
 		key: 'render',
 		value: function render() {
-			return _react3.default.createElement(
+			return _react2.default.createElement(
 				'div',
 				{ ref: 'sticky' },
-				_react3.default.createElement(
+				_react2.default.createElement(
 					'div',
 					null,
 					this.props.children
@@ -285,8 +262,6 @@ var ReactStickyScrollSpy = _wrapComponent('ReactStickyScrollSpy')(function (_Rea
 	}]);
 
 	return ReactStickyScrollSpy;
-}(_react3.default.Component));
+}(_react2.default.Component);
 
 exports.default = ReactStickyScrollSpy;
-
-//# sourceMappingURL=build.js.map
